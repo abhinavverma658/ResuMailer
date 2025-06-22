c20886a53841cc7c17a5333cb50978a2435b8a66
Abhinav Verma <abhinavverma658@gmail.com>
2025-06-22 21:55:18 +0530

    handling cors

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: "10mb" })); // for base64 file
+const allowedOrigin = process.env.ALLOWED_ORIGIN;
+
+app.use(cors({
+  origin: allowedOrigin,
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
+app.use(express.json({ limit: "10mb" }));
 
 app.post("/send-email", async (req, res) => {
   const { from, to, subject, body, password, attachment, filename } = req.body;
